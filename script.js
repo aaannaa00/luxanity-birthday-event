eaa7a60c73c85506c10e0fbb75444eb7d2325544
aaannaa00 <aaannaa00@gmail.com>
2026-07-20 12:20:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 setTimeout(() => {
 
-    document.querySelector(".loading h1").innerHTML =
-    "Authentication Successful";
+    document
+    .getElementById("loadingScreen")
+    .classList.add("hidden");
+
+
+    document
+    .getElementById("loginScreen")
+    .classList.remove("hidden");
+
 
 }, 3500);
+
+
+
+function startEvent(){
+
+    alert("Birthday Event Loading...");
+
+}
