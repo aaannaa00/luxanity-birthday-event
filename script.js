2c84932dd2fef0dba7c55b29b440e6d6c6ee4f2c
aaannaa00 <aaannaa00@gmail.com>
2026-07-20 14:47:36 +0200

    Hextech cursor added

--- script.js
+++ script.js
@@ -199,3 +199,21 @@ Achievement unlocked:
 `;
 
 }
+
+document.addEventListener(
+"mousemove",
+(e)=>{
+
+const cursor =
+document.getElementById("cursor");
+
+
+cursor.style.left =
+e.clientX + "px";
+
+
+cursor.style.top =
+e.clientY + "px";
+
+
+});
